0a3e1f1b743a42f24fdaefbd2ee3fdfdfcc1f919
Julissa Ruales <julissa97_ruales@hotmail.com>
2023-12-06 07:13:55 -0600

    Se agrego el layout

---
+++ Alvarez_Examen2P/obj/Debug/net6.0-android/android/src/crc640258a96dc3ebeb1b/MainActivity.java
@@ -0,0 +1,46 @@
+package crc640258a96dc3ebeb1b;
+
+
+public class MainActivity
+	extends crc6488302ad6e9e4df1a.MauiAppCompatActivity
+	implements
+		mono.android.IGCUserPeer
+{
+/** @hide */
+	public static final String __md_methods;
+	static {
+		__md_methods = 
+			"";
+		mono.android.Runtime.register ("Alvarez_Examen2P.MainActivity, Alvarez_Examen2P", MainActivity.class, __md_methods);
+	}
+
+
+	public MainActivity ()
+	{
+		super ();
+		if (getClass () == MainActivity.class)
+			mono.android.TypeManager.Activate ("Alvarez_Examen2P.MainActivity, Alvarez_Examen2P", "", this, new java.lang.Object[] {  });
+	}
+
+
+	public MainActivity (int p0)
+	{
+		super (p0);
+		if (getClass () == MainActivity.class)
+			mono.android.TypeManager.Activate ("Alvarez_Examen2P.MainActivity, Alvarez_Examen2P", "System.Int32, System.Private.CoreLib", this, new java.lang.Object[] { p0 });
+	}
+
+	private java.util.ArrayList refList;
+	public void monodroidAddReference (java.lang.Object obj)
+	{
+		if (refList == null)
+			refList = new java.util.ArrayList ();
+		refList.add (obj);
+	}
+
+	public void monodroidClearReferences ()
+	{
+		if (refList != null)
+			refList.clear ();
+	}
+}
